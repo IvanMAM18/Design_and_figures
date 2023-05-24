588738888a8ce29ff0b0a67db1251415fba7577d
Ivan Martin Aviles Moreira <ivanaviles569@gmail.com>
2023-05-24 16:51:07 -0700

    Avance

--- design-figures/src/App.vue
+++ design-figures/src/App.vue
@@ -3,13 +3,33 @@
     <div class="row mb-0">
         <Header msg="Hola header"></Header> 
         <div class="col-12 fondo">
-          <button class="boton" type="button"><p class="col letrasBoton">Desings </p><i class="iconoBoton material-icons rigth">expand_more</i></button>
+          <div class="btn-group" role="group" aria-label="Button group dropdown">
+            <div class="btn-group" role="group">
+              <button id="btnGroupDrop1" class="boton" type="button" data-bs-toggle="dropdown" style="font-size:20px; margin-top: 30px; ">
+                <p class="col letrasBoton">Designs </p> <i class="iconoBoton material-icons rigth">expand_more</i>
+              </button>
+              <ul class="dropdown-menu letrasBoton" style="background-color: #A8F0AF;" aria-labelledby="btnGroupDrop1">
+                <li>
+                  <a class="dropdown-item " href="#" style="font-weight: bold;">
+                    Create <i class="material-icons rigth position-absolute end-0 text-end" style="font-size: 1.5em; top:12px; margin-right:10px;">add</i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
           <br><br>
           <div class="row">
-            <div v-for="i in 8" :key="i" class="col-sm-3">
-                <div class="card-body fondoCard ">
-                  <p class="letrasBoton card-title">Title Desing</p>
-                  <img src="./assets/actividadEjemplo.png" class="card-img-top imagenCard" alt="...">
+            <div v-for="i in 8" :key="i" class="col-sm-3 bg-transparent">
+                <div class="card bg-transparent border-0 shadow-none">
+                  <p class="letrasBoton" style="font-size:18px; text-align: justify;">Title Design</p>
+                  <div style="position:relative; bottom:15px;">
+                    <i class="alinearIconoCard position-absolute top-0 end-0 text-end material-icons rigth  ml-auto" style="font-size: 2em;">more_horiz</i>
+                    <img  src="./assets/actividadEjemplo.png" class="card-img-top rounded imagenCard" alt="..." >
+                    <div class="alinearIconoCard position-absolute bottom-0 end-0">
+                      <i class="material-icons rigth">add_comment</i>
+                      <i class="material-icons rigth">favorite</i>
+                    </div>
+                  </div>
                 </div>
             </div>
           </div>
@@ -34,33 +54,36 @@ export default {
 
 <style>
 .fondo{
-    background: #89CD90;
+  background: #E7FFE9;
 }
 .boton{
   width:135px;
   height:40px;
-  background:#89CD90;
-  border: solid #A8F0AF;
+  background:#C3FFC9;
+  border: solid #89CD90;
   border-width: 3px;
   border-radius: 10px;
   margin-top:15px;
 }
+.boton:hover {
+  background-color: #89CD90;
+}
 .letrasBoton{
   font-weight: 100;
-  font-size:22px;
   font-family: Lexend Deca;
+  font-weight: bold;
 }
 .iconoBoton{
   margin-top:5px;
-}
-.fondoCard{
-  background: #89CD90;
 }
 .imagenCard{
   background: #89CD90;
   border: solid #A8F0AF;
   border-width: 3px;
   border-radius: 10px;
+}
+.alinearIconoCard{
+  padding-right: 5px;
 }
 
 </style>
--- design-figures/src/components/Header.vue
+++ design-figures/src/components/Header.vue
@@ -3,8 +3,25 @@
   <div class="row">
     <div class="col-md-5" style="width:100px;"><img src="../assets/logoDyF.png" class="logo"/></div>
     <div class="col-md-3 ms-auto fondoColUser">
-        <p class="col letras"><router-link to="/Login">Iniciar sesion</router-link></p>
-        <div class="col styleUser "><i class="row icono material-icons">person</i></div>
+        <p class="col letras">Usuario</p>
+        <div class="btn-group" role="group" aria-label="Button group dropdown">
+            <div class="btn-group" role="group">
+              <button id="btnGroupDrop1" class="boton" type="button" data-bs-toggle="dropdown" style="font-size:20px; margin-top: 30px; ">
+                <p class="col letrasBoton">Designs </p> <i class="iconoBoton material-icons rigth">expand_more</i>
+              </button>
+              <ul class="dropdown-menu letrasBoton" style="background-color: #A8F0AF;" aria-labelledby="btnGroupDrop1">
+                <li>
+                  <a class="dropdown-item " href="#" style="font-weight: bold;">
+                    Create <i class="material-icons rigth position-absolute end-0 text-end" style="font-size: 1.5em; top:12px; margin-right:10px;">add</i>
+                  </a>
+                  <a class="dropdown-item " href="#" style="font-weight: bold;">
+                    Create <i class="material-icons rigth position-absolute end-0 text-end" style="font-size: 1.5em; top:12px; margin-right:10px;">add</i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        <button type="button" class="col styleUser "><i class="row icono material-icons">person</i></button>
     </div>
   </div>
 </div>
@@ -23,6 +40,8 @@ export default {
 <style scoped>
   .fondo{
     background: #C3FFC9;
+    border-bottom: solid #68D16C;
+    border-width: 1.5px;
     height: 40px;
   }
   .logo{
